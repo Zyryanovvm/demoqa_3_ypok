8964eebf2b1c58ab35c8223d3871c0e4bc602b72
Zyryanovvm <chel.cska11@gmail.com>
2022-04-07 23:00:50 +0500

    Create test, added zoom

--- src/test/java/guru/qa/TextBoxTests.java
+++ src/test/java/guru/qa/TextBoxTests.java
@@ -1,6 +1,7 @@
 package guru.qa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -22,24 +23,25 @@ public class TextBoxTests {
     void fillFormTest() {
         String name = "Vladimir";                               // Задаю переменную name
         open("/automation-practice-form");                      // Перехожу по ссылкe
+        Selenide.zoom(0.75);
         $("#firstName").setValue(name);                         // Ввожу значени переменной Name
         $("#lastName").setValue("Zyryanov");                    // Ввожу данные
         $("#userEmail").setValue("Zyryanovvm@rambler.ru");      // Ввожу данные
         $(byText("Female")).click();                            // Нахожу слово Female и кликаю по нему (Не совсем правильно так делать!)
         $("#userNumber").setValue("89821459787");               // Ввожу данные
         $("#dateOfBirthInput").click();                         // вызываю календарь
-            $(".react-datepicker__month-select").selectOption("January");      // В пиклисте выбираю Месяц
-            $(".react-datepicker__year-select").selectOption("1995");          // В пиклисте выбираю год
-                    $(byText("15")).click();                    // Нахожу на страницу цифру 15 кликаю по нему!!!
+        $(".react-datepicker__month-select").selectOption("January");      // В пиклисте выбираю Месяц
+        $(".react-datepicker__year-select").selectOption("1995");          // В пиклисте выбираю год
+        $(byText("15")).click();                    // Нахожу на страницу цифру 15 кликаю по нему!!!
         $("#subjectsInput").setValue("E").pressEnter();         // Нажимаю на первое слово по букве E
         $(byText("Music")).click();                             // Нахожу слово Female и кликаю по нему!!!
         $("#uploadPicture").uploadFromClasspath("1.png");       // Загружаю файл 1.png из папки Ресурсы
         $("#currentAddress").setValue("Lenina street 11");      // Ввожу данные
         $("#state").click();                                    // Кликаю на пиклист
-            $(byText("NCR")).click();                           // поиск текста NCR и кликаю по нему!!!
+        $(byText("NCR")).click();                           // поиск текста NCR и кликаю по нему!!!
         $("#city").click();                                     // Кликаю на пиклист
-            $(byText("Delhi")).click();                         // поиск текст Delfi и кликаю по нему!!!
-        $("[id=submit]").click();                               // Кликаю на Submit
+        $(byText("Delhi")).click();                         // поиск текст Delfi и кликаю по нему!!!
+        $("#submit").click();                               // Кликаю на Submit
 
         // Проверки корректности внесённых данных
         $(".table-responsive").shouldHave(text("Student Name	" + name + " Zyryanov"),
@@ -49,4 +51,4 @@ public class TextBoxTests {
 
         $("#closeLargeModal").click();                          //Закрываю таблицу как знак успешного теста
     }
-    }
+}
